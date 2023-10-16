f8155e489114d026955be85dbd87f1afa282389d
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-10-16 13:27:42 +0200

    added

---
+++ loginPage/LoginController.js
@@ -0,0 +1,22 @@
+
+
+
+
+/////////////////////////Funksjon som pusher ny bruker inn i databasen
+function makeUser(){
+    let PLACEHOLDER = "dette er en placeholder";
+    let name = document.getElementById('name').value;
+    let password = document.getElementById('password').value;
+    let confirmpassword = document.getElementById('confirmpassword').value;
+
+    model.inputs.addUser.name = name;
+    model.inputs.addUser.password = PLACEHOLDER; /////////// må lage en funksjon som skjekker om "password" og "confirmpassword" er riktig
+
+    
+}
+/////////////////////////
+
+//Funksjon som sjekker om er eksisterende bruker eller ikke i databasen
+function isLogedIn() {
+    
+}
--- loginPage/LoginView.js
+++ loginPage/LoginView.js
@@ -1,4 +1,4 @@
-updateView();
+updateView2();
 
 function updateView() {
     document.getElementById('logIn').innerHTML = /*HTML*/ `
@@ -10,3 +10,33 @@ function updateView() {
     `;
 }
 
+
+function updateView2() {
+    document.getElementById('newUser').innerHTML = /*HTML*/ `
+    <div class="container">
+
+      <div class="loginPosition">
+        <input id="name" type="text" placeholder="NAVN">
+        <input id="password" type="text" placeholder="PASSORD">
+        <input id="confirmpassword" type="text" placeholder="PASSORD">
+      </div>
+
+        <button onclick="makeUser()">LAG BRUKER</button>
+        
+    </div>
+    `;
+}
+
+function updateView3() {
+    document.getElementById('logIn').innerHTML = /*HTML*/ `
+    <div class="container">
+    
+    <div class="loginform"> 
+    <input type="text" id"username" placehoder="brukenavn">
+    <input type="password" id="password" placeholder="passord">
+    </div>
+    <button onclick="()">
+
+    </div>
+    `;
+}
