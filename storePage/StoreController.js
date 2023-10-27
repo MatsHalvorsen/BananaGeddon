163ff35fe03260616fdc6772466780f8e3793dd8
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-10-27 09:31:06 +0200

    last changes

--- storePage/StoreController.js
+++ storePage/StoreController.js
@@ -4,3 +4,28 @@ function getCoins() {
     document.getElementById('coinCount').textContent = coins;
 
 }
+/*
+function skinsToModel(itemnum) {
+    const bruker = model.data.user;
+    const skins = model.data.skins;
+ 
+    let skinToAdd = null;
+ 
+    for (let i = 0; i < skins.length; i++) {
+       if (skins[i].itemnum === itemnum) { 
+          skinToAdd = skins[i];
+          break;
+       }
+    }
+ 
+    if (skinToAdd) {
+        if (!bruker.ownedSkins) {
+          bruker.ownedSkins = []; 
+        } 
+        bruker.ownedSkins.push(skinToAdd);
+        console.log("Skin lagt til");
+     } else {
+        console.log("Skin ikke funnet");
+     }
+  }
+  */
