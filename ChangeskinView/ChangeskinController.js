c8635a47387a01e265af18ae16ef9e0d6ab7bf31
RUSTENT_SVERD <skogensvoktere@hotmail.com>
2023-10-30 12:40:14 +0100

    Update ChangeskinController.js

--- ChangeskinView/ChangeskinController.js
+++ ChangeskinView/ChangeskinController.js
@@ -1,30 +1,27 @@
-
-
-
 function changeSkin(buttonpushed){
     let test
     let userIndex = 0;
-    let equippedSkin = model.data.user[userIndex].equippedSkin;
+    let equippedSkin = model.app.currentUser.equippedSkin;
 
 
         if (buttonpushed == 'left'){
-            model.data.user[userIndex].equippedSkin-=1;
-            if (model.data.user[userIndex].equippedSkin <= 1){
-                model.data.user[userIndex].equippedSkin = 1;
+            model.app.currentUser.equippedSkin-=1;
+            if (model.app.currentUser.equippedSkin <= 1){
+                model.app.currentUser.equippedSkin = 1;
             }
-            test = model.data.user[userIndex].ownedSkins[model.data.user[userIndex].equippedSkin-2];/// "test" er indexen til "ownedSkins", som skal brukes til å velge hvilket skin som er "equippedSkin" 
+            test = model.app.currentUser.ownedSkins[model.app.currentUser.equippedSkin-1];/// "test" er indexen til "ownedSkins", som skal brukes til å velge hvilket skin som er "equippedSkin" 
         } else{
-            model.data.user[userIndex].equippedSkin+=1;
-            if (model.data.user[userIndex].equippedSkin >= model.data.user[userIndex].ownedSkins.length){
-                model.data.user[userIndex].equippedSkin = model.data.user[userIndex].ownedSkins.length;
+            model.app.currentUser.equippedSkin+=1;
+            if (model.app.currentUser.equippedSkin >= model.app.currentUser.ownedSkins.length){
+                model.app.currentUser.equippedSkin = model.app.currentUser.ownedSkins.length;
             }
-            test = model.data.user[userIndex].ownedSkins[model.data.user[userIndex].equippedSkin-2];
+            test = model.app.currentUser.ownedSkins[model.app.currentUser.equippedSkin-1];
         }
     
     
     
 
         console.log(buttonpushed);
-        console.log(model.data.user[0].equippedSkin);
+        console.log(model.app.currentUser.equippedSkin);
         console.log(test);
     }
